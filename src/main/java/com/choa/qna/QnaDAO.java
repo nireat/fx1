85e4efcd3c7e9fd499a753dc0d5516724405237b
user2-PC\user2 <bbokbbok91@gmail.com>
2017-06-30 21:28:32 +0900

    0630

--- src/main/java/com/choa/qna/QnaDAO.java
+++ src/main/java/com/choa/qna/QnaDAO.java
@@ -1,6 +1,7 @@
 package com.choa.qna;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
@@ -14,6 +15,23 @@ public class QnaDAO {
 	private SqlSession sqlSession;
 	private final String namespace="QnaMapper.";
 	
+	public List<QnaDTO> list(int start, int last){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("start", start);
+		map.put("last", last);
+		sqlSession.selectOne(namespace+"list", map);
+		//list --> selectOne으로 많이 호출
+		List<QnaDTO> list=(List<QnaDTO>)map.get("qna");
+		return list;
+	}
+	
+	public QnaDTO view(int num) throws Exception{
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("num", num);
+		sqlSession.selectOne(namespace+"view", map);
+		List<QnaDTO> list=(List<QnaDTO>)map.get("qna");
+		return list.get(0);
+	}
 	
 	public int write(QnaDTO qnaDTO) throws Exception{
 		Map<String, Object> map = new HashMap<String, Object>();
--- src/test/java/com/choa/fx1/QnaTest.java
+++ src/test/java/com/choa/fx1/QnaTest.java
@@ -2,6 +2,8 @@ package com.choa.fx1;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,14 +17,26 @@ public class QnaTest extends MyAbstractTestUnit {
 	
 	@Test
 	public void test() throws Exception{
-		QnaDTO qnaDTO = new QnaDTO();
+		/*QnaDTO qnaDTO = new QnaDTO();
 		qnaDTO.setNum(74);
 		qnaDTO.setTitle("re");
 		qnaDTO.setContents("re");
 		assertEquals(1, qnaDAO.reply(qnaDTO));
 		
-		System.out.println("Reply Test");
+		System.out.println("Reply Test");*/
+		
+		/*QnaDTO qnaDTO=qnaDAO.view(134);
+		System.out.println(qnaDTO.getWriter());*/
+		
+		List<QnaDTO> list = qnaDAO.list(1, 10);
+		for(QnaDTO qnaDTO: list){
+			System.out.println(qnaDTO.getWriter());
+		}
+		
+		
 		
 	}
+
+	
 
 }
